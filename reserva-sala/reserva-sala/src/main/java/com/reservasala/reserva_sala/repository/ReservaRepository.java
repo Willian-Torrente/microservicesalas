3665e5a9ebf0154f6025ed8dd28122c9a39e247a
WilliaTorrente <willian.nacional@gmail.com>
2025-04-07 09:18:32 -0300

    Commit 02 error on primary

--- reserva-sala/reserva-sala/src/main/java/com/reservasala/reserva_sala/repository/ReservaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.reservasala.reserva_sala.repository;
-
-import com.reservasala.reserva_sala.model.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservaRepository extends JpaRepository<Reserva, Long> {}
