a10c0f2811ceeee09408ece0c027248c19ba5c8a
WilliaTorrente <willian.nacional@gmail.com>
2025-06-15 00:00:30 -0300

    Ajustes finais projeto microServices Rodando

--- reservas-microservicos/gatewayService/src/main/java/GatewayApplication.java
+++ reservas-microservicos/gatewayService/src/main/java/GatewayApplication.java
@@ -1,3 +1,4 @@
+// (Removed incorrect package declaration or update as needed)
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
